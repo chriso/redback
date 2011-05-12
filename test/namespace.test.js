4e367c7b87845c4ef1ca64e963496eb27f2717e4
Chris O'Hara <cohara87@gmail.com>
2011-05-12 21:31:54 +1000

    Fixed namespace tests

--- test/namespace.test.js
+++ test/namespace.test.js
@@ -6,13 +6,16 @@ module.exports = {
     'test namespaces': function() {
         var client = redback.createClient(null, null, {namespace:'foo'});
         assert.equal('foo', client.namespace);
-        assert.equal('foo:bar', client.namespaceKey('bar'));
 
         var channel = client.createChannel('channel__');
         assert.equal('foo:channel__', channel.name);
 
         var cache = client.createCache('cache__');
         assert.equal('foo:cache__', cache.namespace);
+
+        var list = client.createList('list__');
+        assert.equal('foo', list.namespace);
+        assert.equal('foo:bar', list.namespaceKey('bar'));
 
         client.client.quit();
     },
