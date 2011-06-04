f8b337181bd9b7ea1c9ecd4a86bb502774c7601d
V Sreekanth <sreeix@gmail.com>
2011-06-05 00:16:08 +0530

    Getting the tests right

--- test/crc32.test.js
+++ test/crc32.test.js
@@ -2,9 +2,13 @@ var crc32 = require('Crc32').crc32,
     assert = require('assert');
 
 module.exports = {
+  'test crc32': function () {
+    console.log(crc32('foo'));
+    assert.equal(-1938594527, crc32('foo'));
+  },
 
-    'test crc32': function () {
-	assert.equals(0x3610A686,crc32('foo'));
-    }
-
+  'test crc32 for 123456789': function () {
+    console.log(crc32('123456789'));
+    assert.equal(-873187034, crc32('123456789'));
+  }
 }
