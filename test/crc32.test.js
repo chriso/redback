18873cef1c89b4678cd4556ca938cae1a4a3209f
V Sreekanth <sreeix@gmail.com>
2011-06-05 05:19:18 +0530

    Implementing  simple bloom filter for redback

--- test/crc32.test.js
+++ test/crc32.test.js
@@ -3,12 +3,10 @@ var crc32 = require('Crc32').crc32,
 
 module.exports = {
   'test crc32': function () {
-    console.log(crc32('foo'));
-    assert.equal(-1938594527, crc32('foo'));
+    assert.equal(1938594527, crc32('foo'));
   },
 
   'test crc32 for 123456789': function () {
-    console.log(crc32('123456789'));
-    assert.equal(-873187034, crc32('123456789'));
+    assert.equal(873187034, crc32('123456789'));
   }
 }
